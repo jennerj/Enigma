ff79af58609d02d6a1696d5794989193711b994f
Jacob Jenner <jennerjacob@gmail.com>
2022-01-15 13:09:25 -0700

    Add: decrypt method

--- spec/Enigma_spec.rb
+++ spec/Enigma_spec.rb
@@ -12,8 +12,17 @@ RSpec.describe Enigma do
   it 'can encrypt message with key and date' do
     expected = {encryption: "keder ohulw",
                 key: "02715",
-                date: "040895"}
+                date: "040895"
+              }
     expect(@enigma.encrypt("hello world", "02715", "040895")).to eq(expected)
+  end
+
+  it 'can decrypt message with key and date' do
+    expected = {decryption: "hello world",
+        key: "02715",
+        date: "040895"
+      }
+      expect(@enigma.decrypt("keder ofulw", "02715", "040895")).to eq(expected)
   end
 
 
