d70d578310a1dd5b4abf569a290bf213d475ab08
Jacob Jenner <jennerjacob@gmail.com>
2022-01-16 15:01:08 -0700

    Write methods offset and offset_group as well as writing new test for keyable spec and corresponding gen_four_keys method

--- spec/offset_spec.rb
+++ spec/offset_spec.rb
@@ -6,8 +6,14 @@ RSpec.describe Offset do
     offset = Offset.new("040895")
   end
 
-  it 'generates offset from the date recieved' do
-    offset = Offset.new("040895")
-    expect(offset.offsets).to eq({"A" => "1", "B" => "0", "C" => "2", "D" => "5"})
+  it 'generates offset numbers from the date recieved' do
+    offset_gen = Offset.new("040895")
+    expect(offset_gen.offset("040895")).to eq("1025")
+  end
+
+  it 'generates offsets A..D using offset numbers' do
+    offset_gen = Offset.new("040895")
+    expect(offset_gen.offset_group).to eq({"A" => "1", "B" => "0", "C" => "2", "D" => "5"})
+
   end
 end
