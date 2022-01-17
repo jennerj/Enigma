61b344a41b3cffefbca38a416c1e5f34cde573a5
Jacob Jenner <jennerjacob@gmail.com>
2022-01-17 14:21:08 -0700

    ADD: shifted_cipher and shifted_message methods into enigma class, its doing the thing
    !!!
    exit
    q
    '

--- lib/Enigma.rb
+++ lib/Enigma.rb
@@ -1,3 +1,4 @@
+require './spec_helper.rb'
 class Enigma
 
   def initialize
@@ -8,8 +9,9 @@ class Enigma
     @offset = Offset.new(date)
     @shift = Shift.new(@keyable.keys, @offset.offsets)
     array = @shift.shifts.values.map {|value| value.to_i}
+    # require "pry"; binding.pry
     encrypted = {}
-    encrypted[:encryption] = y(message, array)
+    encrypted[:encryption] = shifted_message(message, array)
     encrypted[:key] = @keyable.key
     encrypted[:date] = @offset.date
     encrypted
@@ -21,12 +23,79 @@ class Enigma
     @shift = Shift.new(@keyable.keys, @offset.offsets)
     array = @shift.shifts.values.map {|value| value.to_i}
     decrypted = {}
-    decrypted[:decryption] = x(ciphertext, array)
+    decrypted[:decryption] = shifted_cipher(ciphertext, array)
     decrypted[:key] = key
     decrypted[:date] = @offset.date
     decrypted
   end
 
+  def shifted_message(message, shifts_array)
+    #create character set
+    alphabet_array = ("a".."z").to_a << " "
+    #creates encrypted text placeholder and downcases message
+    encrypted_text = ""
+    message.downcase!
+    #cycles through the shifts_array the count of the (keys)
+    shift_loop = shifts_array.cycle(message.length)
+    #each character if it includes letter then proceed, unless special character
+      message.chars.each do |letter|
+        if alphabet_array.include?(letter)
+          #grabs next key out of shift loop
+          shift_letter_amount = shift_loop.next
+          #if letter = a then alphabet_array.index(letter) is 0 + key from above(shift_letter_amount)
+          new_index = alphabet_array.index(letter) + shift_letter_amount
+          # if number passed into new_index is higher than usable index number (ie..27) then reduce till usable
+          if new_index >= 108
+            new_index = new_index - 108
+          end
+          if new_index >= 81
+            new_index = new_index - 81
+          end
+          if new_index >= 54
+            new_index = new_index - 54
+          end
+          if new_index >= 27
+            new_index = new_index - 27
+          end
+          #generates new index value and then pulls encrypted letter from alphabet array
+          new_letter = alphabet_array[new_index]
+          #placeholder encrypted messaged gets letter added to it
+          encrypted_text << new_letter.to_s
+        elsif
+          #if special character dump in
+          encrypted_text << letter.to_s
+        end
+      end
+      encrypted_text
+  end
 
-
+  def shifted_cipher(ciphertext, shifts_array)
+    alphabet_array = ("a".."z").to_a << " "
+    decrypted_text = ""
+    ciphertext.to_s
+    shift_loop = shifts_array.cycle(ciphertext.length)
+      ciphertext.chars.each do |letter|
+        if alphabet_array.include?(letter)
+          shift_letter_amount = shift_loop.next
+          new_index = alphabet_array.index(letter) - shift_letter_amount
+          if new_index < -81
+            new_index = new_index + 108
+          end
+          if new_index < -54
+            new_index = new_index + 81
+          end
+          if new_index < -27
+            new_index = new_index + 54
+          end
+          if new_index < 0
+            new_index = new_index + 27
+          end
+          new_letter = alphabet_array[new_index]
+          decrypted_text << new_letter.to_s
+        elsif
+          decrypted_text << letter
+        end
+      end
+      decrypted_text
+  end
 end
--- spec/Enigma_spec.rb
+++ spec/Enigma_spec.rb
@@ -2,21 +2,11 @@ require './spec_helper.rb'
 
 RSpec.describe Enigma do
   before(:each) do
-    # @keyable = Keyable.new("02715")
-    # @offset = Offset.new("040895")
-    #
-    # @keyable.gen_four_keys
-    # @offset.offset_group
-    #
-    # @shift = Shift.new(@keyable.keys, @offset.offsets)
     @enigma = Enigma.new
-
   end
 
   it 'exists' do
     expect(@enigma).to be_a(Enigma)
-    # expected = {"A" => "3", "B" => "27", "C" => "73", "D" => "20"}
-    # expect(@enigma.sum_of_keys_and_offsets(@keyable.keys, @offset_maker.offsets)).to eq(expected)
   end
 
   it 'can encrypt message with key and date' do
@@ -32,7 +22,7 @@ RSpec.describe Enigma do
         key: "02715",
         date: "040895"
       }
-      expect(@enigma.decrypt("keder ofulw", "02715", "040895")).to eq(expected)
+      expect(@enigma.decrypt("keder ohulw", "02715", "040895")).to eq(expected)
   end
 
   #encrypt using today's date
@@ -44,16 +34,27 @@ RSpec.describe Enigma do
   #decrypts using today's date
   it 'can decrypt using todays date' do
     encrypted = @enigma.encrypt("hello world", "02715")
-    expect(@enigma.decrypt(encrypted[:encryption], [:key])).to be_a(Hash)
-    expect(@enigma.decrypt(encrypted[:encryption], [:key]).count).to eq(3)
+    expect(@enigma.decrypt(encrypted[:encryption], :key)).to be_a(Hash)
+    expect(@enigma.decrypt(encrypted[:encryption], :key).count).to eq(3)
   end
 
   it 'can encrypt and decrypt using a random key' do
     encrypted = @enigma.encrypt("hello world")
     expect(@enigma.decrypt(encrypted[:encryption], encrypted[:key])[:decryption]).to eq("hello world")
   end
+  #tests to make sure that just the message changes after encryption
+  it 'can encrypt a message without changing the other characters' do
+    expected = {encryption: "keder ohulw!",
+                key: "02715",
+                date: "040895"}
+    expect(@enigma.encrypt("hello world!", "02715", "040895")).to eq(expected)
+  end
 
-
-
-
+  #tests to make sure that just the message changes after decryption
+  it 'can decrypt a message without changing the other characters' do
+    expected = {decryption: "hello world!",
+                key: "02715",
+                date: "040895"}
+    expect(@enigma.decrypt("keder ohulw!", "02715", "040895")).to eq(expected)
+  end
 end
