e042b4386ab9f30ba152ded12686d7760a44213d
Jacob Jenner <jennerjacob@gmail.com>
2022-01-16 19:34:32 -0700

    Fix rogue arguement in before each in shift_spec, write sum_of_keys_and_offsets

--- lib/shift.rb
+++ lib/shift.rb
@@ -3,5 +3,10 @@ class Shift
     @keys = keys
     @offsets = offsets
   end
-  
+
+  def sum_of_keys_and_offsets(keys, offsets)
+    shifts = keys.merge(offsets) do |k, keys, offsets|
+      (keys.to_i + offsets.to_i).to_s
+    end
+  end
 end
--- spec/shift_spec.rb
+++ spec/shift_spec.rb
@@ -5,16 +5,16 @@ RSpec.describe Shift do
     @keyable = Keyable.new("02715")
     @offset = Offset.new("040895")
 
-    @keyable.gen_four_keys("02715")
+    @keyable.gen_four_keys
     @offset.offset_group
 
     @shift = Shift.new(@keyable.keys, @offset.offsets)
   end
 
-  # it 'exists' do
-  #   @shift = Shift.new(@keyable.keys, @offset.offsets)
-  #   expect(@shift).to be_instance_of(Shift)
-  # end
+  it 'exists' do
+    @shift = Shift.new(@keyable.keys, @offset.offsets)
+    expect(@shift).to be_instance_of(Shift)
+  end
 
   it 'can add keys and offsets' do
     expected = {"A" => "3", "B" => "27", "C" => "73", "D" => "20"}
